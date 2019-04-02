383008738f6cc2a2466d8ae750fbd4f1e57a8362
Tim White <tim@whiteitsolutions.com.au>
2019-04-02 20:45:44 +1000

    Update package details for a fork

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='Greg',
-    version='0.4.7',
+    name='Greg White',
+    version='0.4.8',
     install_requires=['feedparser'],
     description='A command-line podcast aggregator',
-    author='Manolo Martínez',
-    author_email='manolo@austrohungaro.com',
-    url='https://github.com/manolomartinez/greg',
+    author='Manolo Martínez, Pepie34, Timothy White',
+    author_email='tim@whiteitsolutions.com.au',
+    url='https://github.com/timwhite/greg',
     packages=['greg'],
     entry_points={'console_scripts': ['greg = greg.parser:main']},
     package_data={'greg': ['data/*.conf']},
